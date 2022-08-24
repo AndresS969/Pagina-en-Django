b8d925782fa1e5c397a3210358f17e7c3a3c7d4f
jose <andresbm>
2022-08-24 09:30:07 -0500

    Primer proyecto Django

---
+++ PrimerProyecto/PrimerProyecto/views.py
@@ -0,0 +1,21 @@
+from django.http import HttpResponse
+
+#crear una vista en django: con una fucnion
+
+#vista: es para devolvernos una respuestas
+
+def saludo(request):
+    return HttpResponse("HOLA ESTA ES NUESTRA PRIMERA PAGINA WEB") #devolvernos la respuesta
+
+#debemos solictar una url que nos mostrara la vista
+#debemos enlazar un enlace para que muestre nuestra vista
+
+def Name(request):
+    return HttpResponse("MI NOMBRE ES ANDRES Y ESTA ES MI PAGINA EN DJANGO")
+
+
+def Despedida(request):
+    return HttpResponse("HASTA LUEGO AMIGO")
+
+def OtroSaludo(request):
+    return HttpResponse("ENVIANDO OTRO SALUDO")
